7d60f799125c39b9d49f1ae201f59ac8dfdda96c
andrew <a@example.com>
2018-10-18 16:12:46 +0300

    add SQLAlchemy model and db component

--- app.py
+++ app.py
@@ -2,26 +2,42 @@ import json
 import datetime
 import subprocess
 import test
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask import Flask
 
 
-
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    create_time = db.Column(db.String(128))
+    start_time = db.Column(db.String(128))
+    exec_time = db.Column(db.String(128))
+    def __repr__(self):
+        return ''.format(self.id)
 
 def to_json(data):
     return json.dumps(data) + '\n'
 
 @app.route('/')
 @app.route('/api/tasks', methods=['GET'])
-def get_tasks():
-    create_time = datetime.datetime.now().isoformat()
-    for_json = {'start_time': create_time}
-    print(str(for_json))
-    time_to_execute = test.main()
-    for_json['time_to_execute'] = time_to_execute
-    print(for_json)
-    return str(for_json)
+def gen_tasks():
+    create_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    for_json = {'create_time': create_time}
+    t = Task(create_time= create_time)
+    db.session.add(t)
+    start_time, time_to_execute = test.main()
+    for_json['start_time'] = start_time
+    for_json['time_to_execute'] = '{0} sec'.format(time_to_execute)
+    db.session.commit()
+    return str(t.id)
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=4445)
+    app.run(host='192.168.0.10', port=8083, debug=True)
--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import time
 import random
 
 def main():
-    start_time = time.clock()
+    start_time = time.strftime("%d-%m-%Y %H:%M:%S")
+    str_time = time.time()
     time.sleep(random.randint(0, 10))
-    executable_time = (time.time() - start_time)
+    executable_time = (time.time() - str_time)  // 1
     return start_time, executable_time
 
 if __name__ == '__main__':
-    print(main())
+    main()
