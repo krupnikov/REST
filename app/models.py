c7b48de3c0571564f023fe4ddc51fee067d5aeb0
andrew <a@example.com>
2018-10-26 12:47:09 +0300

    refactoring code

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from app.database import Base
+from datetime import datetime
 
 # Class to describe ORM TASK in db
 
@@ -7,9 +8,9 @@ class Task(Base):
     __tablename__ = 'tasks'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    create_time = Column(DateTime)
+    create_time = Column(DateTime, default=datetime.now())
     start_time = Column(DateTime)
-    exec_time = Column(DateTime)
+    exec_time = Column(Integer)
     status = Column(String(10))
 
     def __init__(self, create_time=None, start_time=None, exec_time=None, status=None):
--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,6 @@ from app import app
 from app.database import db_session
 from app.models import Task
 from app.test import main
-from sqlalchemy import update
 
 @app.teardown_appcontext
 def shutdown_session(exception= None):
@@ -22,41 +21,40 @@ num_worker_threads = 2
 #     def __init__(self, work_queue):
 #
 
-# def worker():
-#     while True:
-#         item = q.get()
-#         do_work(item)
-#         q.task_done()
+def worker():
+    while True:
+        item = q.get()
+        do_work(item)
+        q.task_done()
 
-# def do_work(arg):
-#     start_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-#     s = main()
-#     db_session.execute("UPDATE tasks SET start_time='{0}' WHERE id={1}".format(start_time, arg.id))
-#     db_session.commit()
-#     exec_time = s
-#     db_session.execute("UPDATE tasks SET exec_time='{0}' WHERE id={1}".format(exec_time, arg.id))
-#     db_session.commit()
+def do_work(arg):
+    start_time = datetime.datetime.now()
+    s = main()
+    db_session.execute("UPDATE tasks SET start_time='{0}' WHERE id={1}".format(start_time, arg.id))
+    db_session.commit()
+    exec_time = int(s)
+    db_session.execute("UPDATE tasks SET exec_time='{0}' WHERE id={1}".format(exec_time, arg.id))
+    db_session.commit()
 
-# def gen_workers():
-#     for i in range(num_worker_threads):
-#         trd = threading.Thread(target=worker)
-#         trd.start()
-#         threads.append(trd)
+def gen_workers():
+    for i in range(num_worker_threads):
+        trd = threading.Thread(target=worker)
+        trd.start()
+        threads.append(trd)
 
 
 @app.route('/task', methods=['PUT'])
 def gen_tasks():
-    create_time = datetime.datetime.now()
-    t = Task(create_time=create_time, status='In Queue')
-    db_session.add(t)
+    t = Task(status='In Queue')
     q.put(t)
+    db_session.add(t)
     db_session.commit()
-    # gen_workers()
+    gen_workers()
     return str(t.id)
 
 @app.route('/task/info/<int:id>', methods=['GET'])
 def get_task_info(id):
-    que = db_session.query(Task).filter(id == id).first()
+    que = db_session.query(Task).filter(Task.id == id).first()
     out = {'status': que.status,
            'create_time': str(que.create_time),
            'start_time': str(que.start_time),
